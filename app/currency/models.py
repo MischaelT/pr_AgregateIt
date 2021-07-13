b3acf2d86b5de409d7c3d2678bd9cf6090f44d3c
michael <m.d.kouzin@gmail.com>
2021-07-13 13:21:19 +0300

    Created models in currency app

---
+++ app/currency/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Rate(models.Model):
+    ask = models.DecimalField(max_digits=4, decimal_places=2)
+    bid = models.DecimalField(max_digits=4, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    sourсe = models.CharField(max_length=16) #оставшиеся симфолы заменяются пробелами
+    type = models.CharField(max_length=3)
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=32)
+    subject = models.CharField(max_length= 128)
+    message = models.TextField()
---
+++ app/currency/views.py
@@ -0,0 +1,7 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+def hello_world(request):
+    return HttpResponse('Hello world')
+
+
