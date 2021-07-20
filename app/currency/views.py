a2f1c094fb6df0c2d2a30affd7bef8a8e581a1fb
michael <m.d.kouzin@gmail.com>
2021-07-20 07:35:48 +0300

    Added Travis and flake

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,5 +1,26 @@
 from django.http.response import HttpResponse
+from django import request
+
+from app.currency.models import Rate
+
+from django.shortcuts import render
 
 
 def hello_world(request):
     return HttpResponse('Hello world')
+
+
+def rate_list(requests):
+    rates = Rate.objects.all()
+    result = []
+
+    for rate in rates:
+        result.append(
+            f'id: {rate.id} ask: {rate.ask} bid: {rate.bid}'
+        )
+
+        context = {
+            'message' : 'hello'
+        }
+
+    return render(request, 'rate_list.html', context=context)
