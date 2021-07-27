bb2c19b546cfddff41ecf7be27ff4f5bdd6992ab
michael <m.d.kouzin@gmail.com>
2021-07-27 10:02:14 +0300

    7th homework: added base template, footer template and make rates and contact us in table appearance

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,5 +1,4 @@
-from currency.views import get_contact_us
-from currency.views import hello_world
+from currency.views import get_contact_us, index, rate_list
 
 from django.contrib import admin
 from django.urls import path
@@ -7,7 +6,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', hello_world),
-    path('get-contactUs/', get_contact_us),
+    path('index/', index),
+    path('contactUs/', get_contact_us),
+    path('rate/list/', rate_list),
 
 ]
